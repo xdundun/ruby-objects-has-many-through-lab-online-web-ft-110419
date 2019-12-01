37b54f01b8d72c2661f2768cb5fb722d1130fbc5
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-01 20:40:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,9 +15,9 @@ class Artist
     Song.all.select {|song| song.artist == self }
   end
 
-   def new_song(name,genre)
-     Song.new(name,self,genre)
-   end
+  def new_song(name,genre)
+    Song.new(name,self,genre)
+  end
 
    def genres
     songs.map {|song| song.genre}
