4499fb287b248dfa578ef1ac91334b2591257131
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-01 20:19:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,7 @@ class Artist
     Song.all.select {|song| song.artist == self }
   end
 
-  # def new_song(name,artist,genre)
+   def new_song(name,genre)
   #   Song.new(name,self,genre)
   # end
 
