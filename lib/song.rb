2ab5214a73e6b504eb435bb0d3f40e3c752b50d6
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-01 20:18:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def name(name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
