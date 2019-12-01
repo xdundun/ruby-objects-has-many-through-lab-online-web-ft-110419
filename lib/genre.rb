594bb51817283afe77f11b70389eacee1b31573d
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-01 20:02:55 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -10,17 +10,7 @@ class Genre
       @@all
     end
 
-    # def add_song(song)
-    #   @songs << song
-    #   song.genre = self
-    # end
-
-    # def songs
-    #   @songs
-    # end
-
-    # def artists
-    #   @songs.collect { |song| song.artist}
-    # end
+    def songs
+    end
 
 end #end of class
